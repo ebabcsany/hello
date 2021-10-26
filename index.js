b6ba635ccd1d363791fd925a4dac928d5b331ba8
Babcsány Ervin <ervin@hinfox.com>
2021-10-26 16:38:29 +0200

    1 added, 1 modified (hello\index.js & hello\.idea\vcs.xml)

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-function onRequest(request, response){
+function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello world');
     response.end();
